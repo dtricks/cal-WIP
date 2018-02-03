74d3592b2980edeadcaafe214bf2a040c054ca97
C4RL0zZo <32815292+C4RL0zZo@users.noreply.github.com>
2018-02-03 12:48:07 +0100

    add empty activities and layouts

--- app/src/main/java/com/example/reset/food_database/activity_tagebuch.java
+++
@@ -1,8 +0,0 @@
-package com.example.reset.food_database;
-
-/**
- * Created by Denis on 03.02.2018.
- */
-
-public class activity_tagebuch {
-}
---
+++ app/src/main/java/com/example/reset/food_database/diary/activity_diary.java
@@ -0,0 +1,8 @@
+package com.example.reset.food_database.diary;
+
+/**
+ * Created by Denis on 03.02.2018.
+ */
+
+public class activity_diary {
+}
